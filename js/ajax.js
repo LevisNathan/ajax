54dd04c939b31f3d0488dca19f695806e1b0a1ac
Nathan K Levis <levisnathan@gmail.com>
2017-12-03 10:02:01 +0000

    all

---
+++ js/ajax.js
@@ -0,0 +1,14 @@
+    $.ajax({
+                type: "get",
+                url: "",
+                dataType: "",
+                data: { },
+                success: function(data,status) {
+                    //alert(data); 
+                    
+                    
+                  },
+                complete: function(data,status) { //optional, used for debugging purposes
+                     //alert(status);
+                }
+             });
