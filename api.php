33561747ad3ad5d364c86973a04b751b4bd09557
Nathan K Levis <levisnathan@gmail.com>
2017-12-03 10:10:32 +0000

    all

--- api.php
+++ api.php
@@ -3,12 +3,12 @@
 
 
 function getDatabaseConnection() {
- 
+ mysql://b8677ab705078a:70af7eb4@us-cdbr-iron-east-05.cleardb.net/heroku_2201c4dbaea3547?reconnect=true
     return $dbConn; 
       $host = "us-cdbr-iron-east-05.cleardb.net";
-    $username = "b4038f71e14ee4";
-    $password = "227621f2";
-    $dbname = "heroku_22106684c98b3b9"; 
+    $username = "b8677ab705078a";
+    $password = "70af7eb4";
+    $dbname = "heroku_2201c4dbaea3547"; 
     
     
     // Create connection
